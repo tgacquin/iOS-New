1df304e13fb02bfd88bb4cc2a3dea8a17e94c51a
ben <ben@MobileProject.local>
2017-04-12 21:12:46 -0400

    okay

--- controlCenter/RadioTest/RadioTest/Scraper.swift
+++ controlCenter/RadioTest/RadioTest/Scraper.swift
@@ -10,25 +10,14 @@ import Foundation
 import Alamofire
 import Kanna
 
-Alamofire.request(.GET, "http://wmuc.umd.edu")
-    .responseString { response in
-        print("Response String: \(response.result.value)")
+class scraper {
+
+    func refresh(){
+        
+        if let doc = Kanna.HTML(url: (NSURL(string: "http://wmuc.umd.edu")!) as URL, encoding: .utf8) {
+            
+            print(doc);
+            
+    }
 }
-
-let HTML = response.result.value
-
-if let doc = HTML(html: html, encoding: .utf8) {
-    print(doc.title)
-    
-    // Search for nodes by CSS
-    for link in doc.css("a, link") {
-        print(link.text)
-        print(link["href"])
-    }
-    
-    // Search for nodes by XPath
-    for link in doc.xpath("//a | //link") {
-        print(link.text)
-        print(link["href"])
-    }
 }
